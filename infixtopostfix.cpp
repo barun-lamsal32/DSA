906baa85a8a4947b39c78065501e0c61d9722127
barun-lamsal32 <barunlamsal516@gmail.com>
2026-07-21 07:12:41 +0545

    Enhance infix to postfix conversion with operator precedence

--- infixtopostfix.cpp
+++ infixtopostfix.cpp
@@ -4,54 +4,62 @@
 #include <stack>
 using namespace std;
 
-int precedence(char ch){
-    if (ch=='/' || ch=='*' || ch=='%') return 2;
-    if( ch=='+' || ch=='-') return 1;
+int precedence(char ch) {
+    if (ch == '^') return 3;
+    if (ch == '/' || ch == '*' || ch == '%') return 2;
+    if (ch == '+' || ch == '-') return 1;
     return 0;
 }
 
-bool isoperator (char ch){
-    return ch=='+' || ch=='-' ||
-    ch=='/' || ch=='*' ||ch=='%' ;
+bool isoperator(char ch) {
+    return ch == '+' || ch == '-' ||
+           ch == '/' || ch == '*' ||
+           ch == '%' || ch == '^';
 }
 
 string infixtopostfix(string infix) {
-    stack<char>st;
-    string postfix="";
+    stack<char> st;
+    string postfix = "";
 
-    for(char ch: infix){
-        if(isalnum(ch)){
-            postfix+=ch;
+    for (char ch : infix) {
+        if (isalnum(ch)) {
+            postfix += ch;
         }
-        else if(ch=='('){
+        else if (ch == '(') {
             st.push(ch);
         }
-        else if(ch==')'){
-            while(!st.empty() && st.top()!='('){
-                postfix+=st.top();
+        else if (ch == ')') {
+            while (!st.empty() && st.top() != '(') {
+                postfix += st.top();
                 st.pop();
             }
-            if(!st.empty()){
-                st.pop();
-             // Remove '('
+            if (!st.empty()) {
+                st.pop();   // Remove '('
             }
         }
-        else if(isoperator(ch)){
-            while(!st.empty() && st.top()!='(' && precedence(st.top())>=precedence(ch)){
-                postfix+=st.top();
+        else if (isoperator(ch)) {
+
+            while (!st.empty() && st.top() != '(' &&
+                  (precedence(st.top()) > precedence(ch) ||
+                  (precedence(st.top()) == precedence(ch) && ch != '^'))) {
+
+                postfix += st.top();
                 st.pop();
             }
+
             st.push(ch);
         }
     }
-    while(!st.empty()){
-        postfix+=st.top();
+
+    while (!st.empty()) {
+        postfix += st.top();
         st.pop();
     }
+
     return postfix;
 }
 
-int main(){
+int main() {
     string infix;
 
     cout << "Enter infix expression: ";
